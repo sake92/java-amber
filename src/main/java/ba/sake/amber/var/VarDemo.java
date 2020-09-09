820711b58efe47560da7cc405dd390195eda0019
Sakib Hadžiavdić <sake.etf@gmail.com>
2020-09-09 19:27:49 +0200

    Add diamond operator example

--- src/main/java/ba/sake/amber/var/VarDemo.java
+++ src/main/java/ba/sake/amber/var/VarDemo.java
@@ -2,6 +2,7 @@ package ba.sake.amber.var;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
@@ -21,6 +22,10 @@ public class VarDemo {
 
         var integer = 123;
         System.out.println(integer);
+
+        var list = new ArrayList<>();
+        list.add("abc");
+        list.add(123);
 
 
         // in lambdas:
