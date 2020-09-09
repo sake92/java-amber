5d5968e6c7d85071370e87f4f3556a7073fe24ed
Sakib Hadžiavdić <sake.etf@gmail.com>
2020-09-09 17:39:59 +0200

    Update shape comment

--- src/main/java/ba/sake/amber/sealed/Shape.java
+++ src/main/java/ba/sake/amber/sealed/Shape.java
@@ -1,5 +1,9 @@
 package ba.sake.amber.sealed;
 
+/**
+ * - subtypes must be one of: <b>final, non-sealed, sealed</b> <br>
+ * - can't be used <b>outside of package</b>, current limitation
+ */
 sealed class Shape {}
 
 final class Circle extends Shape {}
